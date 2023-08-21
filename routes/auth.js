8eca4cb785f56728d68a39805b405ee8d532c13b
Argiro <elto.82@gmai.com>
2023-08-21 11:26:45 -0500

    endpoints auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,18 @@
 // host + /api/auth
 
 const express = require("express");
+const {
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+} = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+router.post("/new", crearUsuario);
+
+router.post("/", loginUsuario);
+
+router.get("/renew", revalidarToken);
 
 module.exports = router;
