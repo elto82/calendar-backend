2290c7d46c1a855a54ea9fd3e838d647d2bef1b3
Argiro <elto.82@gmai.com>
2023-08-22 09:15:33 -0500

    custom middleware de validacion de datos

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,14 @@
 const { check } = require("express-validator");
-
-// Rutas de Usuarios / Auth
-// host + /api/auth
-
 const express = require("express");
 const {
   crearUsuario,
   loginUsuario,
   revalidarToken,
 } = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validarCampos");
+
+// Rutas de Usuarios / Auth
+// host + /api/auth
 
 const router = express.Router();
 
@@ -20,6 +20,7 @@ router.post(
     check("password", "El password debe ser minimo de 6 caracteres").isLength({
       min: 6,
     }),
+    validarCampos,
   ],
   crearUsuario
 );
@@ -31,6 +32,7 @@ router.post(
     check("password", "El password debe ser minimo de 6 caracteres").isLength({
       min: 6,
     }),
+    validarCampos,
   ],
   loginUsuario
 );
