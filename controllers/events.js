e2bb5f9e4e47c6b71fbc7f589519203a596b5341
Argiro <elto.82@gmai.com>
2023-08-25 11:14:38 -0500

    actualizar evento

--- controllers/events.js
+++ controllers/events.js
@@ -28,11 +28,43 @@ const crearEvento = async (req, res = response) => {
 };
 
 const actualizarEvento = async (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "actualizarEvento",
-  });
+  const eventId = req.params.id;
+  const uid = req.uid;
+  try {
+    const event = await Event.findById(eventId);
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Evento no encontrado",
+      });
+    }
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene privilegio de editar este evento",
+      });
+    }
+    const newEvent = {
+      ...req.body,
+      user: uid,
+    };
+    const eventUpdated = await Event.findByIdAndUpdate(eventId, newEvent, {
+      new: true,
+    });
+
+    res.json({
+      ok: true,
+      event: eventUpdated,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error inesperado",
+    });
+  }
 };
+
 const eliminarEvento = async (req, res = response) => {
   res.json({
     ok: true,
